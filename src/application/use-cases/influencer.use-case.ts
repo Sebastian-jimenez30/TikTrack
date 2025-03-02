7020e7a5123f34819a7a02d9bdbc2c6d249434a8
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-03-02 17:30:49 -0500

    refactor: move code from controllers to use-cases

--- src/application/use-cases/influencer.use-case.ts
+++ src/application/use-cases/influencer.use-case.ts
@@ -1,14 +1,26 @@
 import { Influencer } from "@/domain/entities/influencer";
 import IInfluencerRepository from "@/application/repositories/influencer.repository.interface";
 import repositoryContainer from "~/containers/repository.container";
+import PaginationUtil from "@/interface-adapters/utils/pagination";
 export class InfluencerUseCases {
-  async list(pageNumber: number, limit: number): Promise<Influencer[]> {
+  async listWithPagination(
+    pageNumber: number,
+    limit: number
+  ): Promise<{
+    influencers: Influencer[];
+    count: number;
+    start: number;
+    end: number;
+    hasNextPage: boolean;
+    hasPreviousPage: boolean;
+  }> {
     const repository = repositoryContainer.get<IInfluencerRepository>(
       "IInfluencerRepository"
     );
-    const response = await repository.listPaginated(pageNumber, limit);
+    const tempInfluencers = await repository.listPaginated(pageNumber, limit);
+    const tempCount = await repository.count();
 
-    const influencers = response.map((influencer) => {
+    const influencers = tempInfluencers.map((influencer) => {
       return new Influencer(
         influencer.id,
         influencer.username,
@@ -27,17 +39,22 @@ export class InfluencerUseCases {
         influencer.updatedAt
       );
     });
-    return influencers;
-  }
-  async count(): Promise<number> {
-    const repository = repositoryContainer.get<IInfluencerRepository>(
-      "IInfluencerRepository"
+
+    const count = Number(tempCount);
+    const [start, end] = PaginationUtil.getIndexes(
+      pageNumber.toString(),
+      count,
+      limit
     );
 
-    let response = await repository.count();
-    response = Number(response);
-
-    return response;
+    return {
+      influencers,
+      count,
+      start,
+      end,
+      hasNextPage: end < count,
+      hasPreviousPage: start > 1,
+    };
   }
 }
 export const influencerUseCases = new InfluencerUseCases();
