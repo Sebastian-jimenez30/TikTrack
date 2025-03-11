35f39752e4130479c640c5b4e00d40ad988012ab
Sebastian Jimenez <anderjime864@gmail.com>
2025-03-11 00:35:28 -0500

    feat(auth): implement sign-up an sign-in page

--- src/shared/utils/jwt.util.ts
+++ src/shared/utils/jwt.util.ts
@@ -1,4 +1,3 @@
-// src/infrastructure/services/jwt.service.ts
 import jwt from "jsonwebtoken";
 
 const SECRET = process.env.JWT_SECRET || "secret";
@@ -10,12 +9,10 @@ interface TokenPayload {
 }
 
 export class JwtService {
-  // Generar un token JWT
   generateToken(payload: TokenPayload): string {
     return jwt.sign(payload, SECRET, { expiresIn: "1h" });
   }
 
-  // Verificar un token JWT
   verifyToken(token: string): TokenPayload {
     return jwt.verify(token, SECRET) as TokenPayload;
   }
