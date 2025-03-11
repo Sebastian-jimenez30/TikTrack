6482c6d811e800b334758e06b1f966184ee3cb48
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-03-11 18:46:33 -0500

    refactor: change service to util

--- src/shared/utils/jwt.util.ts
+++ src/shared/utils/jwt.util.ts
@@ -1,19 +1,25 @@
 import jwt from "jsonwebtoken";
 
-const SECRET = process.env.JWT_SECRET || "secret";
-
 interface TokenPayload {
-  userId: number; 
+  userId: number;
   email?: string;
   role?: string;
 }
 
-export class JwtService {
+class JwtUtil {
+  secret: string;
+
+  constructor() {
+    this.secret = process.env.JWT_SECRET || "secret";
+  }
   generateToken(payload: TokenPayload): string {
-    return jwt.sign(payload, SECRET, { expiresIn: "1h" });
+    return jwt.sign(payload, this.secret, { expiresIn: "1h" });
   }
 
   verifyToken(token: string): TokenPayload {
-    return jwt.verify(token, SECRET) as TokenPayload;
+    return jwt.verify(token, this.secret) as TokenPayload;
   }
 }
+
+const jwtUtil = new JwtUtil();
+export default jwtUtil;
