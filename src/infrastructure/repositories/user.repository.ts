90925ba4c6d43119648a8efdd9a9553f1ab8dfa3
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-03-11 18:45:39 -0500

    refactor: delete unused methods

--- src/infrastructure/repositories/user.repository.ts
+++ src/infrastructure/repositories/user.repository.ts
@@ -8,8 +8,8 @@ export default class UserRepository implements IUserRepository {
     email: string;
     password: string;
     name: string;
-    role?: "admin" | "user"; 
-    status?: "active" | "inactive"; 
+    role?: "admin" | "user";
+    status?: "active" | "inactive";
   }): Promise<{
     id: number;
     email: string;
@@ -19,33 +19,18 @@ export default class UserRepository implements IUserRepository {
     createdAt: Date;
     updatedAt: Date;
   }> {
-
     const newUser = await db
       .insert(usersTable)
       .values({
-        email: user.email, 
-        password: user.password, 
-        name: user.name, 
-        role: user.role || "user", 
-        status: user.status || "active", 
+        email: user.email,
+        password: user.password,
+        name: user.name,
+        role: user.role || "user",
+        status: user.status || "active",
       })
       .returning();
 
     return newUser[0];
-  }
-
-  async findUserById(id: number): Promise<{
-    id: number;
-    email: string;
-    password: string;
-    name: string;
-    role: "admin" | "user";
-    status: "active" | "inactive";
-    createdAt: Date;
-    updatedAt: Date;
-  } | null> {
-    const user = await db.select().from(usersTable).where(eq(usersTable.id, id));
-    return user[0] || null;
   }
 
   async findUserByEmail(email: string): Promise<{
@@ -58,34 +43,10 @@ export default class UserRepository implements IUserRepository {
     createdAt: Date;
     updatedAt: Date;
   } | null> {
-    const user = await db.select().from(usersTable).where(eq(usersTable.email, email));
+    const user = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.email, email));
     return user[0] || null;
-  }
-
-  async updateProfile(
-    id: number,
-    data: Partial<{
-      email: string;
-      password: string;
-      name: string;
-    }>
-  ): Promise<{
-    id: number;
-    email: string;
-    name: string;
-    role: "admin" | "user";
-    status: "active" | "inactive";
-    createdAt: Date;
-    updatedAt: Date;
-  }> {
-
-
-    const updatedUser = await db
-      .update(usersTable)
-      .set(data)
-      .where(eq(usersTable.id, id))
-      .returning();
-
-    return updatedUser[0];
   }
 }
