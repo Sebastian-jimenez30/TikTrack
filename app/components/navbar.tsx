13682c0725345363630bbac3739dbe3e0037f987
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-03-11 18:50:15 -0500

    feat: use cookies to manage button navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,21 +1,15 @@
-"use client"; 
-
 import { useTranslations } from "next-intl";
 import { Link } from "~/i18n/routing";
 import Image from "next/image";
 import Button from "./button";
 import ROUTES from "~/constants/urls";
-import { JSX, useEffect, useState } from "react";
 
-export default function NavBar(): JSX.Element {
+interface NavBarProps {
+  isAuthenticated: boolean;
+}
+
+export default function NavBar({ isAuthenticated }: NavBarProps) {
   const t = useTranslations("NavBar");
-
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(() => {
-    const token = localStorage.getItem("authToken"); 
-    setIsAuthenticated(!!token); 
-  }, []);
 
   return (
     <nav className="bg-white border-gray-200">
@@ -30,34 +24,11 @@ export default function NavBar(): JSX.Element {
         />
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <Button
-            href={isAuthenticated ? ROUTES["HOME"] : ROUTES["SIGN_IN"]} 
+            href={isAuthenticated ? ROUTES["HOME"] : ROUTES["SIGN_IN"]}
             variant="primary"
           >
-            {isAuthenticated ? t("profile") : t("getStarted")} 
+            {isAuthenticated ? t("profile") : t("getStarted")}
           </Button>
-          <button
-            data-collapse-toggle="navbar-cta"
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-            aria-controls="navbar-cta"
-            aria-expanded="false"
-          >
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
         </div>
         <div
           className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
