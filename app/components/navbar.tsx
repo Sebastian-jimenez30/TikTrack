56e939533317f7a17ab6b755362404081b4eccea
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-03-12 14:50:19 -0500

    fix: add hamburguer button and margin

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -17,7 +17,7 @@ export default function NavBar({ isAuthenticated, locale }: NavBarProps) {
   const t = useTranslations("NavBar");
 
   return (
-    <nav className="bg-white border-gray-200">
+    <nav className="bg-white border-gray-200 mb-10">
       <div className="max-w-screen-xl flex flex-wrap items-center sm:justify-between mx-auto p-4 justify-center">
         <Image
           src="/logos/combination-mark.png"
@@ -30,13 +30,10 @@ export default function NavBar({ isAuthenticated, locale }: NavBarProps) {
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse items-center">
           {isAuthenticated ? (
             <div className="flex items-center gap-4 bg-gray-100 p-2 rounded-md">
-              {/* Botón de perfil */}
               <Button href={ROUTES.PROFILE} variant="primary">
                 {t("profile")}
               </Button>
-              {/* Separador */}
               <div className="border-l border-gray-300 h-6"></div>
-              {/* Botón de logout */}
               <form action={() => logout(locale)}>
                 <button
                   type="submit"
@@ -52,6 +49,29 @@ export default function NavBar({ isAuthenticated, locale }: NavBarProps) {
               {t("getStarted")}
             </Button>
           )}
+          <button
+            data-collapse-toggle="navbar-cta"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+            aria-controls="navbar-cta"
+            aria-expanded="false"
+          >
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
         </div>
         <div
           className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
