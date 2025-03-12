b357ae6873a17b2fbd477f5d7938d744507e7bee
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-03-11 23:58:20 -0500

    feat: add service to connect with influencer management system

--- src/application/services/influencerManagement.service.interface.ts
+++ src/application/services/influencerManagement.service.interface.ts
@@ -1,18 +1,20 @@
 interface IInfluencerManagementService {
-    addInfluencers(): Promise<{
-        username: string;
-        profileName: string;
-        profilePicture: string;
-        profileUrl: string;
-        averageLikes: number;
-        averageComments: number;
-        averageShares: number;
-        averageSaves: number;
-        averageViews: number;
-        followers: number;
-        city: string;
-        featuredVideos: string[];
-    }[]>;
+  fetchInfluencers(): Promise<
+    {
+      username: string;
+      profileName: string;
+      profilePicture: string;
+      profileUrl: string;
+      averageLikes: number;
+      averageComments: number;
+      averageShares: number;
+      averageSaves: number;
+      averageViews: number;
+      followers: number;
+      city: string;
+      featuredVideos: string[];
+    }[]
+  >;
 }
 
 export default IInfluencerManagementService;
