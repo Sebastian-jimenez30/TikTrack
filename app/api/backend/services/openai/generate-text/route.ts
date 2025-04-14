0c194ed4e8bae1411daf93942e0c12cdcccc3064
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-04-14 18:23:17 -0500

    feat: add dependencies inversion to openai service

--- app/api/backend/services/openai/generate-text/route.ts
+++ app/api/backend/services/openai/generate-text/route.ts
@@ -1,12 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
-import { OpenAIService } from "@/infrastructure/services/openai.service";
+import { IOpenAIService } from "@/application/services/openai.service.interface";
+import serviceContainer from "~/containers/service.container";
 import PROMPTS from "~/constants/prompts";
+
 export async function POST(req: NextRequest) {
     const { input } = await req.json();
     const contextPrompt = PROMPTS.IMPROVE_TEXT;
 
-    const openaiService = new OpenAIService();
-
-    const response = await openaiService.generateText(input, contextPrompt);
+    const service = serviceContainer.get<IOpenAIService>(
+        "IOpenAIService"
+    );
+    const response = await service.generateText(input, contextPrompt);
     return NextResponse.json(response);
 }
---
+++ containers/service.container.ts
@@ -0,0 +1,14 @@
+import "reflect-metadata";
+
+import { Container } from "inversify";
+
+import { IOpenAIService } from "@/application/services/openai.service.interface";
+import { OpenAIService }  from "@/infrastructure/services/openai.service";
+
+const container = new Container();
+
+container
+  .bind<IOpenAIService>("IOpenAIService")
+  .to(OpenAIService);
+
+export default container;
