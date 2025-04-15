cbc9bb22e62cf01003a437994518b346a75b24a6
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-04-14 23:34:05 -0500

    refactor: change login and signup pages to get data with API

--- app/[locale]/(auth)/sign-in/page.tsx
+++ app/[locale]/(auth)/sign-in/page.tsx
@@ -1,16 +1,16 @@
-import { authController } from "@/interface-adapters/controllers/auth.controller";
 import AuthenticationCard from "~/app/components/cards/authentication.card";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 import ROUTES from "~/constants/urls/urls";
-
+import axios from "axios";
+import ROUTES_API from "~/constants/urls/api.urls";
 export default function SignInPage() {
   async function handleLogin(formData: FormData): Promise<{ error?: string }> {
     "use server";
 
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
-    const pageData = await authController.logIn(email, password);
+    const pageData = (await axios.post(ROUTES_API.LOGIN, {email: email, password: password})).data.pageData;
     if (!pageData.is_success) {
       return { error: pageData.message };
     } else {
