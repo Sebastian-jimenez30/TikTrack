84ea33c2a017a1cf5a7a206c570a8f1b59a30a4f
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-04-15 09:17:52 -0500

    feat: move openai improve message to message page

--- app/[locale]/messages/page.tsx
+++ app/[locale]/messages/page.tsx
@@ -1,7 +1,12 @@
-import { getTranslations } from "next-intl/server";
 import { messageController } from "~/src/interface-adapters/controllers/message.controller";
 import MessageList from "~/app/components/message.list";
 import CreateMessage from "~/app/components/forms/create.message";
+import TextboxWithService from "~/app/components/forms/ai.textbox";
+import FireIcon from "~/app/components/icons/fire.icon";
+import HeartIcon from "~/app/components/icons/heart.icon";
+import EyeIcon from "~/app/components/icons/eye.icon";
+import InlineCard from "~/app/components/cards/inline.card";
+import { getTranslations } from "next-intl/server";
 
 export default async function Index() {
   const t = await getTranslations("MessagesPage");
@@ -20,6 +25,33 @@ export default async function Index() {
       </h3>
       {messages.length < 3 && <CreateMessage />}
       <MessageList messages={messages} />
+
+      <div className="mx-[100px]">
+        <section>
+          <h2 className="mb-8 text-3xl text-center font-bold leading-none tracking-tight md:text-4xl md:text-center lg:text-4xl lg:text-left ">
+            {t("aiSuggestions")}
+          </h2>
+          <div className="flex flex-col flex-wrap justify-center lg:flex-row gap-6">
+            <div className="flex flex-1 justify-center md:justify-center lg:justify-start flex-col">
+              <TextboxWithService />
+            </div>
+            <div className="flex flex-1 justify-center flex-col items-center flex-wrap ml-5 my-5 lg:my-0 gap-6">
+              <InlineCard
+                icon={<FireIcon className="text-5xl mr-5 text-lightPurple" />}
+                description={t("communicationDescription")}
+              />
+              <InlineCard
+                icon={<HeartIcon className="text-5xl mr-5 text-lightPurple" />}
+                description={t("connectionDescription")}
+              />
+              <InlineCard
+                icon={<EyeIcon className="text-5xl mr-5 text-lightPurple" />}
+                description={t("standOutDescription")}
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
