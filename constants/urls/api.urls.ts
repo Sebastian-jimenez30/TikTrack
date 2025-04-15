3a9be03347facb690c292efefa13908fbc93cb14
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-04-15 13:23:50 -0500

    feat: add deactivate api url

--- constants/urls/api.urls.ts
+++ constants/urls/api.urls.ts
@@ -6,6 +6,8 @@ const ROUTES_API = {
   HOME_INDEX: NEXT_PUBLIC_BASE_URL_API + "/api/backend/home/index",
   INFLUENCER_INDEX: NEXT_PUBLIC_BASE_URL_API + "/api/backend/influencer/index",
   INFLUENCER_SHOW: NEXT_PUBLIC_BASE_URL_API + "/api/backend/influencer/show",
+  INFLUENCER_DEACTIVATE:
+    NEXT_PUBLIC_BASE_URL_API + "/api/backend/influencer/deactivate",
   LOGIN: NEXT_PUBLIC_BASE_URL_API + "/api/backend/auth/login",
   SIGN_UP: NEXT_PUBLIC_BASE_URL_API + "/api/backend/auth/sign-up",
   PROFILE_SHOW: NEXT_PUBLIC_BASE_URL_API + "/api/backend/profile/show",
