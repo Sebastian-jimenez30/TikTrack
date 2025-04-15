bdc891dc30a4f6d44672707a10bca34265f4e329
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-04-15 13:25:11 -0500

    feat: add new repository function to update influencer status

--- src/application/repositories/influencer.repository.interface.ts
+++ src/application/repositories/influencer.repository.interface.ts
@@ -1,5 +1,7 @@
+import { Status } from "@/domain/entities/influencer";
+
 export default interface IInfluencerRepository {
-  listPaginated(
+  listActivePaginated(
     pageNumber: number,
     limit: number
   ): Promise<
@@ -17,6 +19,7 @@ export default interface IInfluencerRepository {
       followers: number;
       city: string;
       featuredVideos: string[];
+      status: Status;
       createdAt: Date;
       updatedAt: Date;
     }[]
@@ -35,10 +38,13 @@ export default interface IInfluencerRepository {
     followers: number;
     city: string;
     featuredVideos: string[];
+    status: Status;
     createdAt: Date;
     updatedAt: Date;
   } | null>;
-  count(): Promise<number>;
+
+  countActive(): Promise<number>;
+  countInactive(): Promise<number>;
 
   create(influencer: {
     username: string;
@@ -53,5 +59,24 @@ export default interface IInfluencerRepository {
     followers: number;
     city: string;
     featuredVideos: string[];
+  }): Promise<void>;
+
+  update(influencer: {
+    id: number;
+    username: string;
+    profileName: string;
+    profilePicture: string;
+    profileUrl: string;
+    averageLikes: number;
+    averageComments: number;
+    averageShares: number;
+    averageSaves: number;
+    averageViews: number;
+    followers: number;
+    city: string;
+    featuredVideos: string[];
+    status: Status;
+    createdAt: Date;
+    updatedAt: Date;
   }): Promise<void>;
 }
