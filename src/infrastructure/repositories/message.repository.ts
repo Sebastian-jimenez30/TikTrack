762f1e9a2549c98c0bae53473d2c2746e22d492b
Evelyn <eazapatat@eafit.edu.co>
2025-05-24 19:56:30 -0500

    feat: order messages by ID in listAll

--- src/infrastructure/repositories/message.repository.ts
+++ src/infrastructure/repositories/message.repository.ts
@@ -2,6 +2,7 @@ import { eq } from "drizzle-orm";
 import { messagesTable } from "@/infrastructure/database/schemas/message.schema";
 import IMessageRepository from "@/application/repositories/message.repository.interface";
 import db from "@/infrastructure/database/index";
+import { asc } from "drizzle-orm";
 
 export default class MessageRepository implements IMessageRepository {
   async listAll(): Promise<
@@ -12,7 +13,7 @@ export default class MessageRepository implements IMessageRepository {
       updated_at: Date;
     }[]
   > {
-    return await db.select().from(messagesTable);
+    return await db.select().from(messagesTable).orderBy(asc(messagesTable.id));
   }
 
   async create(message: { content: string }): Promise<{
